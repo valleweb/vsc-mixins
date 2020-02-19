73271f5ffc9e16b30e0866e8ac470ffe3719502d
Larissa Abreu <larissa.a.abreu@hotmail.com>
2020-02-18 22:00:22 -0300

    update to Polymer 3

---
+++ test/valle-mixinButton/fake-button.js
@@ -0,0 +1,14 @@
+﻿import { valleMixinButton } from '../../valle-mixinButton.js';
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+
+class fakeButton extends valleMixinButton(PolymerElement) {
+  static get template() {
+    return html`
+      <template>
+        <slot></slot>
+      </template>
+    `
+  }
+};
+
+window.customElements.define('fake-button', fakeButton);
